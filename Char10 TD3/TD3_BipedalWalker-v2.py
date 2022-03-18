9df3aff767d029b86e5eb323e6e724d287418b33
SiriusSchrodinger <hw.yang@berkeley.edu>
2022-03-17 23:11:23 -0700

    trying to init

--- Char10 TD3/TD3_BipedalWalker-v2.py
+++ Char10 TD3/TD3_BipedalWalker-v2.py
@@ -19,7 +19,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--mode', default='train', type=str) # mode = 'train' or 'test'
 # OpenAI gym environment name, # ['BipedalWalker-v2', 'Pendulum-v0'] or any continuous environment
 # Note that if you want test in another game, you should fine-tuning.
-parser.add_argument("--env_name", default="BipedalWalker-v2")
+parser.add_argument("--env_name", default="BipedalWalker-v3")
 parser.add_argument('--tau',  default=0.005, type=float) # target smoothing coefficient
 parser.add_argument('--target_update_interval', default=1, type=int)
 parser.add_argument('--test_iteration', default=10, type=int)
